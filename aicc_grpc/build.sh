e91580c516b6e04bfc65c97d1f8cc8bad5b71df7
LeeYangseung <dldidtmd1@naver.com>
2022-05-09 15:45:16 +0900

    first commit

---
+++ aicc_grpc/build.sh
@@ -0,0 +1,15 @@
+IMAGE_NAME="aicc_grpc"
+VERSION="1.0"
+ARCHES="x86_64"
+PLATFORM_ARG=`printf '%s ' '--platform'; for var in $(echo $ARCHES | sed "s/,/ /g"); do printf 'linux/%s,' "$var"; done | sed 's/,*$//g'`
+
+# gRPC base image 생성
+docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/${IMAGE_NAME}/${VERSION}/base"
+
+# aia용 빌드 이미지 생성
+cp -r /root/go/src/grpc_aia dist/${IMAGE_NAME}/${VERSION}/aia/source
+docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" "dist/${IMAGE_NAME}/${VERSION}/aia"
+
+# adt용 빌드 이미지 생성
+cp -r /root/go/src/grpc_adt dist/${IMAGE_NAME}/${VERSION}/adt/source
+docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" "dist/${IMAGE_NAME}/${VERSION}/adt"

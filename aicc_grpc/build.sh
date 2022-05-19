67b2d6d16126e668275bc79fe0d0dc6f3525621d
LeeYangseung <dldidtmd1@naver.com>
2022-05-19 10:04:43 +0900

    버전 관리 및 stt서버 베이스 추가

--- aicc_grpc/build.sh
+++ aicc_grpc/build.sh
@@ -1,5 +1,5 @@
 IMAGE_NAME="aicc_grpc"
-VERSION="1.0"
+VERSION="1.1"
 ARCHES="x86_64"
 PLATFORM_ARG=`printf '%s ' '--platform'; for var in $(echo $ARCHES | sed "s/,/ /g"); do printf 'linux/%s,' "$var"; done | sed 's/,*$//g'`
 
@@ -8,8 +8,8 @@ docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/$
 
 # aia용 빌드 이미지 생성
 cp -r /root/go/src/grpc_aia dist/${IMAGE_NAME}/${VERSION}/aia/source
-docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" "dist/${IMAGE_NAME}/${VERSION}/aia"
+docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/${IMAGE_NAME}/${VERSION}/aia"
 
 # adt용 빌드 이미지 생성
 cp -r /root/go/src/grpc_adt dist/${IMAGE_NAME}/${VERSION}/adt/source
-docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" "dist/${IMAGE_NAME}/${VERSION}/adt"
+docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/${IMAGE_NAME}/${VERSION}/adt"

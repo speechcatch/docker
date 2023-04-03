17891d6170187f58168b08d6452fbe7d622ee300
LeeYangseung <dldidtmd1@naver.com>
2023-04-03 09:31:44 +0900

    1.42

--- aicc_tts_server/build.sh
+++ aicc_tts_server/build.sh
@@ -12,24 +12,25 @@ PLATFORM_ARG=`printf '%s ' '--platform'; for var in $(echo $ARCHES | sed "s/,/ /
 # aia용 빌드 이미지 생성
 if [ "$DIST" = "aia" ]; then
   # TTS base image 생성
-  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base_centos/${VERSION}"
+  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base_centos/${VERSION}" --no-cache
 
   rm -rf dist/${DIST}/${VERSION}/source/
   git clone -b aia --single-branch https://github.com/speechclone/speechclone_8k_inf.git dist/${DIST}/${VERSION}/source
   git clone -b aia --single-branch https://github.com/speechclone/korean_normalizer.git dist/${DIST}/${VERSION}/source/speechclone_tts/utils/korean_normalizer
+  git clone https://gitlab.com/skdt/aistudio/accuaicc/e2hsk/e2hsk.git dist/${DIST}/${VERSION}/source/speechclone_tts/utils/korean_normalizer/e2hsk
 
-  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION"=${VERSION} "dist/${DIST}/${VERSION}"
+  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME"=${IMAGE_NAME} --build-arg "VERSION"=${VERSION} "dist/${DIST}/${VERSION}" --no-cache
 fi
 
 
 # adt용 빌드 이미지 생성
 if [ "$DIST" = "adt" ]; then
   # TTS base image 생성
-  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base/${VERSION}"
+  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base/${VERSION}" --no-cache
 
   rm -rf dist/${DIST}/${VERSION}/source/
   git clone -b adt --single-branch https://github.com/speechclone/speechclone_8k_inf.git dist/${DIST}/${VERSION}/source
   git clone -b adt --single-branch https://github.com/speechclone/korean_normalizer.git dist/${DIST}/${VERSION}/source/speechclone_tts/utils/korean_normalizer
 
-  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/${DIST}/${VERSION}" --no-cache
+  docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME"=${IMAGE_NAME} --build-arg "VERSION"=${VERSION} "dist/${DIST}/${VERSION}" --no-cache
 fi

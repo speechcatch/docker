ad9fd37a5b8b0740d69c60af0e6bec849fc8da92
LeeYangseung <dldidtmd1@naver.com>
2023-06-01 10:24:57 +0900

    update

--- aicc_grpc/build.sh
+++ aicc_grpc/build.sh
@@ -11,11 +11,11 @@ PLATFORM_ARG=`printf '%s ' '--platform'; for var in $(echo ${ARCHES} | sed "s/,/
 
 echo "Build ${DIST} ${VERSION}"
 
-# gRPC base image 생성
-docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base/${VERSION}"
-
-# aia용 빌드 이미지 생성
 if [ "$DIST" = "aia" ]; then
+    # gRPC base image 생성
+    docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base/${VERSION}"
+
+    # aia용 빌드 이미지 생성
     rm -rf dist/${DIST}/${VERSION}/source/*
     rm -rf dist/${DIST}/${VERSION}/source/.git*
     git clone -b aia --single-branch https://github.com/LeeYangseung/grpc_go.git dist/${DIST}/${VERSION}/source
@@ -27,8 +27,12 @@ if [ "$DIST" = "aia_fromsource" ]; then
     docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_aia:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/aia/${VERSION}"
 fi
 
-# adt용 빌드 이미지 생성
+
 if [ "$DIST" = "adt" ]; then
+    # gRPC base image 생성
+    docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}:${VERSION}" "dist/base/${VERSION}"
+
+    # adt용 빌드 이미지 생성
     rm -rf dist/${DIST}/${VERSION}/source/*
     rm -rf dist/${DIST}/${VERSION}/source/.git*
     git clone -b adt --single-branch https://github.com/LeeYangseung/grpc_go.git dist/${DIST}/${VERSION}/source
@@ -37,4 +41,15 @@ fi
 
 if [ "$DIST" = "adt_fromsource" ]; then
     docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_adt:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/adt/${VERSION}"
+fi
+
+if [ "$DIST" = "onnx" ]; then
+    # gRPC base image 생성
+    docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_centos:${VERSION}" "dist/base_centos/${VERSION}"
+
+    # onnx용 빌드 이미지 생성
+    rm -rf dist/${DIST}/${VERSION}/source/*
+    rm -rf dist/${DIST}/${VERSION}/source/.git*
+    git clone https://github.com/LeeYangseung/grpc_go.git dist/${DIST}/${VERSION}/source
+    docker buildx build --load ${PLATFORM_ARG} -t "${IMAGE_NAME}_onnx:${VERSION}" --build-arg "IMAGE_NAME=${IMAGE_NAME}" --build-arg "VERSION=${VERSION}" "dist/${DIST}/${VERSION}"
 fi
